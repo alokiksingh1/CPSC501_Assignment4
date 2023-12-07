4ee3e413af520e0a7b2a595affefcfc920946cce
alokiksingh1 <alokiksingh.kinra@ucalgary.ca>
2023-12-06 18:10:31 -0700

    testing for fft, memory not allocating error

--- fft_convolution.c
+++ fft_convolution.c
@@ -8,27 +8,6 @@ This program will do convolution in frequency domain.
 #include <math.h>
 #define SWAP(a,b)  tempr=(a);(a)=(b);(b)=tempr
 
-// function declaration
-void pad_zeros_to(double *arr, int current_length, int M);
-void writeWAVHeader(int numChannels, int numSamples, int outputRate, int bitsPerSample, FILE *outputFile);
-size_t fwriteShortLSB(short int data, FILE *stream);
-size_t fwriteIntLSB(int data, FILE *stream);
-void four1(double data[], int nn, int isign);
-void reverse_array(double *arr, int length);
-int next_power_of_2(int n);
-void convolution(double *x, int K, double *h, double *y) ;
-void readWav(const char *filename, double **data, int *length);
-int convolveTone(char* sampleTone, char* impulseTone, char* outputTone);
-
-// Function to pad zeros to the input array to make its length M
-void pad_zeros_to(double *arr, int current_length, int M) {
-    int padding = M - current_length;
-    for (int i = 0; i < padding; ++i) {
-        arr[current_length + i] = 0.0;
-    }
-}
-
-
 // struct to hold all data up until the end of subchunk1
 typedef struct {
     char chunk_ID[4];           // "RIFF"
@@ -44,11 +23,30 @@ typedef struct {
     short bitsPerSample;       // 8 bits = 8, 16 bits = 16, etc.
     char subChunk2ID[4];
     int subChunk2Size;
-
-    char* dataArray;
-    short* signal;
-    int signalSize;
 } WavHeader;
+
+// function declaration
+void pad_zeros_to(double *arr, int current_length, int M);
+void writeWAVHeader(int numChannels, int numSamples, int outputRate, int bitsPerSample, FILE *outputFile);
+size_t fwriteShortLSB(short int data, FILE *stream);
+size_t fwriteIntLSB(int data, FILE *stream);
+void four1(double data[], int nn, int isign);
+void reverse_array(double *arr, int length);
+int next_power_of_2(int n);
+void convolution(double *x, int K, double *h, double *y) ;
+void readWav(double **data, int *length, WavHeader *header, FILE *file);
+int convolveTone(char* sampleTone, char* impulseTone, char* outputTone);
+
+// Function to pad zeros to the input array to make its length M
+void pad_zeros_to(double *arr, int current_length, int M) {
+    int padding = M - current_length;
+    for (int i = 0; i < padding; ++i) {
+        arr[current_length + i] = 0.0;
+    }
+}
+
+
+
 
 void writeWAVHeader(int numChannels, int numSamples, int outputRate, int bitsPerSample, FILE *outputFile){
 
@@ -196,6 +194,7 @@ int main (int argc, char *argv[]){
 
     if (argc != 4){
         fprintf(stderr, "Usage : %s \t3 arguments required: (input file) (IR file) (output file)\n", argv[0]);
+        exit(-1);
     }
 
     inputFile = argv[1];
@@ -209,47 +208,46 @@ int main (int argc, char *argv[]){
 }
 
 
-void readWav(const char *filename, double **data, int *length) {
-    FILE *file = fopen(filename, "rb");
-    if (!file) {
-        perror("Error opening file");
-        exit(1);
-    }
-
+void readWav(double **data, int *length, WavHeader *header, FILE *file) {
     // Read the header
-    WavHeader header;
-    fread(&header, sizeof(WavHeader), 1, file);
+
+    fread(header, sizeof(WavHeader), 1, file);
 
     // Ensure the WAV file is of the expected format
-    if (header.audioFormat != 1 || header.numChannels != 1 || header.bitsPerSample != 16) {
+    if (header->audioFormat != 1 || header->numChannels != 1 || header->bitsPerSample != 16) {
         fprintf(stderr, "Unsupported WAV format\n");
         fclose(file);
-        exit(1);
+        exit(-1);
     }
 
     // Calculate the number of samples
-    *length = header.subChunk2Size / (header.numChannels * (header.bitsPerSample / 8));
+    *length = header->subChunk2Size / (header->numChannels * (header->bitsPerSample / 8));
 
     // Allocate memory for the audio data
     *data = (double *)malloc(*length * sizeof(double));
     if (*data == NULL) {
         perror("Error allocating memory for audio data");
         fclose(file);
-        exit(1);
+        exit(-1);
     }
 
     // Read the audio data and convert to floating point
     for (int i = 0; i < *length; ++i) {
-        short sample;
-        if (fread(&sample, sizeof(short), 1, file) < 1) {
+        short shortSample;  // random initialization
+        size_t readFile = fread(&shortSample, sizeof(short), 1, file);
+        
+        if (readFile< 1) {
             perror("Error reading audio data");
             free(*data);
             fclose(file);
-            exit(1);
+            exit(-1);
         }
-        // Normalize 16-bit PCM data to [-1.0, 1.0]
-        (*data)[i] = sample / 32767.0;
+        (*data)[i] = (double)shortSample / 32767.0;
     }
+    // for (int i = 0; i < *length; i++) {
+    //     // Normalize 16-bit PCM data to [-1.0, 1.0]
+    //     (*data)[i] = 32767.0;
+    // }
 
     fclose(file);
 }
@@ -259,7 +257,7 @@ void readWav(const char *filename, double **data, int *length) {
 int convolveTone(char* sampleTone, char* impulseTone, char* outputTone) {
     FILE *inputFile = fopen(sampleTone, "rb");
     FILE *IRfile = fopen(impulseTone, "rb");
-    FILE *outputFile = fopen(outputTone, "wb");
+    //FILE *outputFile = fopen(outputTone, "wb");
 
     if (inputFile == NULL){
         printf("Couldn't load input file, Error!!\n");
@@ -271,18 +269,22 @@ int convolveTone(char* sampleTone, char* impulseTone, char* outputTone) {
         perror("");
         exit(-1);
     }
-    if (outputFile == NULL){
-        printf("Couldn't load ouput file, Error!!\n");
-        perror("");
-        fclose(inputFile);
-        exit(-1);
-    }
+    
 
     
     double *inputSignal, *IRsignal;
     int inputLength, IRlength;
-    readWav(sampleTone, &inputSignal, &inputLength);
-    readWav(impulseTone, &IRsignal, &IRlength);
+
+    WavHeader inputHeader, IRheader;
+    // fread(&inputHeader, sizeof(WavHeader), 1, inputFile);
+    // fread(&IRheader, sizeof(WavHeader), 1, IRfile);
+
+    printf("Reading input and IR files...\n");
+    readWav(&inputSignal, &inputLength, &inputHeader, inputFile);
+    printf("input length: %d\n", inputLength);
+    printf("IR length: %d\n", IRlength);
+    readWav(&IRsignal, &IRlength, &IRheader, IRfile);
+    printf("IR length: %d\n", IRlength);
 
     // size needed for fft
     int M = inputLength + IRlength - 1; 
@@ -325,23 +327,40 @@ int convolveTone(char* sampleTone, char* impulseTone, char* outputTone) {
     for (int i = 0; i < 2*K; i=i+2) {
         printf("%f ", y[i]);
     }
-    // Write to output WAV file
-    WavHeader inputHeader;
+
     int numChannels = inputHeader.numChannels;
     int bitsPerSample = inputHeader.bitsPerSample;
     int outputRate = inputHeader.sampleRate;
 
+    FILE *outputFile = fopen(outputTone, "wb");
+    
+    if (outputFile == NULL){
+        printf("Couldn't load ouput file, Error!!\n");
+        perror("");
+        fclose(inputFile);
+        free(inputSignal);
+        free(IRsignal);
+        exit(-1);
+    }
+
     writeWAVHeader(numChannels, M, outputRate, bitsPerSample , outputFile);
 
-    fwrite(y, sizeof(double), K, outputFile);
+    for (int i = 0; i < K; i++) {
+        short int sample = (short int)(y[2 * i] * 32767.0); // Convert double to 16-bit sample
+        fwriteShortLSB(sample, outputFile);
+    }
 
     // Clean up
-    fclose(inputFile);
-    fclose(IRfile);
-    fclose(outputFile);
+    free(inputSignal);
+    free(IRsignal);
     free(x);
     free(h);
     free(y);
+
+    fclose(inputFile);
+    fclose(IRfile);
+    fclose(outputFile);
+    
 
     return 0;
 }
